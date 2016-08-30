5418f7f80d82c649707fb39e74efb144468eec20
Adrian Quintana <adrianquintana@gmail.com>
2016-08-30 07:30:07 +0100

    initial load from personal repo

---
+++ jupyter-frontend/RunControl.py
@@ -0,0 +1,75 @@
+from IPython.display import display
+import GeppettoLibrary as G
+import GeppettoNeuron
+
+from neuron import h
+h.load_file("stdrun.hoc")
+
+def showRunControlPanel():
+    # Init Panel (Init + Button)
+    #initButton = G.addButton('Init(mV)', ['h.stdinit()'])
+    #initTextfield = G.addTextField('v_init')
+    # TODO This can be another API
+    #initPanel = G.addPanel('Init Panel')
+    #initPanel.addChild(initButton)
+    #initPanel.addChild(initTextfield)
+    #initPanel.setDirection('row')
+    
+    # Init Panel
+    initPanel = G.addTextFieldAndButton("Init (mV)", 'v_init', True, ['h.stdinit()'])
+    
+    # Init Run Button
+    initRunButton = G.addButton('Init & Run', ['h.run()'])    
+    
+    # Stop Button
+    stopButton = G.addButton('Stop')
+    stopButton.on_click(['h.stoprun = 1'])   
+    
+    # Continue til
+    continueTilPanel = G.addTextFieldAndButton("Continue til (ms)", 'runStopAt', True, ['h.continuerun(runStopAt)', 'h.stoprun=1'])
+
+    # Continue for
+    continueForPanel = G.addTextFieldAndButton("Continue for (ms)", 'runStopIn', True, ['h.continuerun(t + runStopIn)', 'h.stoprun=1'])
+    
+    # Single Step
+    singleStepButton = G.addButton('Single Step', ['h.steprun()'])
+    
+    # t Panel
+    timePanel = G.addTextFieldAndButton("t (ms)", 't', False, [])
+    
+    # TStop Panel
+    stopPanel = G.addTextFieldAndButton("Tstop (ms)", 'tstop', True, ['h.tstop_changed()'])
+   
+    # dt Panel
+    dtPanel = G.addTextFieldAndButton("dt (ms)", 'dt', True, ['h.setdt()'])
+    
+    # Points plotted Panel
+    pointsPlottedPanel = G.addTextFieldAndButton("Points plotted/ms", 'steps_per_ms', True, ['h.setdt()'])
+
+    # Scrn update invl Panel
+    scrnUpdateInvlPanel = G.addTextFieldAndButton("Scrn update invl (s)", 'screen_update_invl', True, [])
+    
+    # Real Time Texfield
+    realTimePanel = G.addTextFieldAndButton("Real Time (s)", 'realtime', False, [])
+
+    # Init main panel
+    runControlPanel = G.addPanel('Run Control', items = [initPanel, initRunButton, stopButton, continueTilPanel, continueForPanel, singleStepButton, timePanel, stopPanel, dtPanel, pointsPlottedPanel, scrnUpdateInvlPanel, realTimePanel])
+    
+    display(runControlPanel)    
+    
+    
+# Refresh value every 1 
+class Event(object):
+    def __init__(self):
+        self.fih = h.FInitializeHandler(1, self.callback)
+
+    def callback(self) :
+        for key,value in G.sync_values.items():
+            value.sync_value = str(eval("h._ref_t."+key))
+
+        h.cvode.event(h.t + 1, self.callback)
+
+e = Event()
+
+    
+    

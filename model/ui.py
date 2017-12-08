d238bb51c7fdb5a03306b4403e5c41ff8b46d339
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2017-12-08 13:13:07 +0000

    Handle multiple SVGs

--- model/ui.py
+++ model/ui.py
@@ -1,6 +1,14 @@
 import StringIO
 
 def getSVG(fig):
+    if isinstance(fig, list):
+        svgs = []
+        for f in fig:
+            svgs.append(getSingleSVG(f))
+        return svgs
+    return getSingleSVG(fig)
+
+def getSingleSVG(fig):
     imgdata = StringIO.StringIO()
     fig.savefig(imgdata, format='svg', transparent=True)
     imgdata.seek(0)  # rewind the data

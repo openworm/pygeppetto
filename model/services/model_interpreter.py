19ceb623283bfe5ca15f6d2fed4e34d2e5865f41
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2018-04-03 15:46:21 -0700

    Add Model Interpreter class

---
+++ model/services/model_interpreter.py
@@ -0,0 +1,22 @@
+class ModelInterpreter():
+
+    def __init__(self):
+        pass
+
+    def importType(self, url, typeName, library, commonLibraryAccess):
+        pass
+
+    def importValue(self, importValue):
+        pass
+
+    def downloadModel(self, pointer, format, aspectConfiguration):
+        pass
+
+    def getSupportedOutputs(self, pointer):
+        pass
+
+    def getName(self):
+        pass
+
+    def getDependentModels(self):
+        pass

b841dbba5d35fe7165b48a2733e6b8fc0e471afd
Adrian Quintana <adrianquintana@gmail.com>
2018-09-07 14:26:09 +0100

    make it compatible with py3

--- pygeppetto/ui.py
+++ pygeppetto/ui.py
@@ -19,5 +19,6 @@ def getSingleSVG(fig):
     imgdata = StringIO()
     fig.savefig(imgdata, format='svg', transparent=True)
     imgdata.seek(0)  # rewind the data
-    svg_dta = imgdata.buf  # this is svg data
+    # svg_dta = imgdata.buf  # this is svg data
+    svg_dta = imgdata.getvalue()  # this is svg data
     return svg_dta

c26b02187effc896ea43d4a68ed0b1cac839c16f
Adrian Quintana <adrianquintana@gmail.com>
2018-10-05 11:27:19 +0100

    remove comment

--- pygeppetto/ui.py
+++ pygeppetto/ui.py
@@ -19,6 +19,5 @@ def getSingleSVG(fig):
     imgdata = StringIO()
     fig.savefig(imgdata, format='svg', transparent=True)
     imgdata.seek(0)  # rewind the data
-    # svg_dta = imgdata.buf  # this is svg data
     svg_dta = imgdata.getvalue()  # this is svg data
     return svg_dta

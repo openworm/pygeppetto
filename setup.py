c1dff3fb86c7e592c18a25f00a71e5bcc8e47b9c
Filippo Ledda <filippo@metacell.us>
2019-11-11 22:52:53 +0100

    #43 Updated python version in setup.py

--- setup.py
+++ setup.py
@@ -30,12 +30,9 @@ setup(
     extras_require={'testing': ['pytest', 'responses']},
     classifiers=[
         "Programming Language :: Python",
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
         "Topic :: Software Development",
